a948b5b733fcb4e16e18446237e6b2dc59e251d5
DinkyKum <dinky2912@gmail.com>
2024-12-28 22:32:07 +0530

    Exploring Routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,45 @@
 const express= require('express');
 const app= express();
 
-app.use((req,res)=>{
-    res.send("Main");
-}) 
+app.get("/ab?c", (req, res)=>{
+    res.send("? Testing");
+})
+
+app.get("/ab*c", (req, res)=>{
+    res.send("* Testing");
+})
+
+app.get("/ab+c", (req, res)=>{
+    res.send("+ Testing");
+})
+
+app.get("/a(bc)+d", (req, res)=>{
+    res.send("Grouped Testing");
+})
+
+app.get(/a/, (req, res)=>{
+    res.send("/a/ Testing");
+})
+
+app.get(/.*fly$/, (req, res)=>{
+    res.send("Regex Testing");
+})
+
+app.get("/user", (req, res)=>{
+    res.send("Get request");
+})
+
+app.post("/user", (req, res)=>{
+    res.send("Post request");
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("Delete request");
+})
+
 
 app.use("/Hello", (req,res)=>{
     res.send("Hello");
-})
-
-app.use("/Test", (req,res)=>{
-    res.send("Test");
 })
 
 
